5eb5a61bcffddff33803c1e58ff87d147e939499
Ismoil4ik <zenarasidov7@gmail.com>
2024-08-29 22:21:41 +0500

    Done

---
+++ script.js
@@ -0,0 +1,23 @@
+let ism = prompt("ismingizni kiriting");
+
+console.log("ismimgiz:", ism);
+
+let familiya = prompt("Familiyangizni kiriting");
+
+console.log("Familiyangiz:", familiya);
+
+let yosh = prompt("Yoshingizni kiriting");
+
+console.log("Yoshingiz:", yosh);
+
+let fan = prompt("Yoqtirgan faningizni kiriting");
+
+console.log("Faningiz:", fan);
+
+let tuman = prompt("Yashaydigan tumaningizi kiriting");
+
+console.log("Yashayotgan tumaningiz:", tuman);
+
+alert('Malumotlaringizni kiritganingiz uchun tashakkur')
+
+ 
